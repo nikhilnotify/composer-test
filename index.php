8737efedbc35d3ed44561e3238e36aee8257ffe6
nikhil tiwari <nikhil.t@notifyvisitors.com>
2019-09-25 11:12:38 +0530

    init commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+
+include_once 'vendor/autoload.php';
+
+use Test\Test;
+
+class CheckTest {
+    public function index (){
+        echo __CLASS__ . "<br>";
+        (new Test)->output();
+    }
+}
+
+
+(new CheckTest)->index();
---
+++ src/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Test;
+
+class Test {
+
+    public function output() {
+        echo __CLASS__;
+    }
+
+}
